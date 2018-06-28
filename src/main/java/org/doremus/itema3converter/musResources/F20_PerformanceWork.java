f78bb76a5edf61d09397c7f3cc76400ed2ec66cb
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2018-06-28 18:02:32 +0200

    is about - fix #13

--- src/main/java/org/doremus/itema3converter/musResources/F20_PerformanceWork.java
+++ src/main/java/org/doremus/itema3converter/musResources/F20_PerformanceWork.java
@@ -1,8 +1,18 @@
 package org.doremus.itema3converter.musResources;
 
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.DC;
 import org.apache.jena.vocabulary.RDF;
-import org.doremus.itema3converter.files.Omu;
+import org.apache.jena.vocabulary.RDFS;
+import org.doremus.itema3converter.ConstructURI;
+import org.doremus.itema3converter.files.*;
+import org.doremus.ontology.CIDOC;
 import org.doremus.ontology.FRBROO;
+import org.doremus.ontology.MUS;
+
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class F20_PerformanceWork extends DoremusResource {
 
@@ -11,4 +21,50 @@ public class F20_PerformanceWork extends DoremusResource {
     this.resource.addProperty(RDF.type, FRBROO.F20_Performance_Work);
   }
 
+  public F20_PerformanceWork(Item item) {
+    super(item);
+
+    // Performance: General Topic
+    List<ItemIdxFile> generalTopics = ItemIdxNc.byItem(item.getId());
+    generalTopics.addAll(ItemIdxRencontre.byItem(item.getId()));
+    for (ItemIdxFile t : generalTopics) {
+      try {
+        String uri = ConstructURI.build("rfi", "theme", t.getIdxId()).toString();
+
+        Resource topic = model.createResource(uri)
+          .addProperty(RDF.type, CIDOC.E1_CRM_Entity)
+          .addProperty(DC.identifier, t.getIdxId())
+          .addProperty(RDFS.label, t.getIdxLabel());
+        this.resource.addProperty(CIDOC.P129_is_about, topic);
+
+      } catch (URISyntaxException e) {
+        e.printStackTrace();
+      }
+    }
+
+    // Performance: Geo Topic
+    for (ItemIdxGeo t : ItemIdxGeo.byItem(item.getId())) {
+      E53_Place p = new E53_Place(t.getLieuGeoId());
+      this.resource.addProperty(MUS.U22_is_about_place, p.asResource());
+    }
+
+    // Performance: Person + Corporate Body
+    // create list of persons
+    List<Resource> actors = ItemIdxPersonne.byItem(item.getId()).stream()
+      .map(it -> new E21_Person(it.getIdxId()).asResource())
+      .collect(Collectors.toList());
+
+    // add corporate bodies
+    actors.addAll(ItemIdxMorale.byItem(item.getId()).stream()
+      .map(it -> new F11_Corporate_Body(it.getIdxId()).asResource())
+      .collect(Collectors.toList()));
+
+
+    for (Resource x : actors)
+      this.resource.addProperty(MUS.U21_is_about_actor, x.asResource());
+  }
+
+  public void add(F25_PerformancePlan plan) {
+    this.asResource().addProperty(FRBROO.R9_is_realised_in, plan.asResource());
+  }
 }
--- src/main/java/org/doremus/itema3converter/musResources/F31_Performance.java
+++ src/main/java/org/doremus/itema3converter/musResources/F31_Performance.java
@@ -1,11 +1,12 @@
 package org.doremus.itema3converter.musResources;
 
 import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.vocabulary.DCTerms;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
-import org.doremus.itema3converter.ConstructURI;
-import org.doremus.itema3converter.files.*;
+import org.doremus.itema3converter.files.Invite;
+import org.doremus.itema3converter.files.Item;
+import org.doremus.itema3converter.files.ItemThLieuGeo;
+import org.doremus.itema3converter.files.MagContenu;
 import org.doremus.ontology.CIDOC;
 import org.doremus.ontology.FRBROO;
 
@@ -15,7 +16,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class F31_Performance extends DoremusResource {
   final static List<Integer> performanceProfession = Arrays.asList(13, 205, 2, 10, 7);
@@ -28,11 +28,14 @@ public class F31_Performance extends DoremusResource {
     this.resource.addProperty(RDF.type, FRBROO.F31_Performance);
 
     this.plan = new F25_PerformancePlan(mag);
+    F20_PerformanceWork work = new F20_PerformanceWork(item);
+
     this.resource.addProperty(FRBROO.R25_performed, this.plan.asResource());
-    this.model.add(this.plan.getModel());
+    work.add(this.plan);
+
+    this.model.add(this.plan.getModel()).add(work.getModel());
 
     // Performance: Date
-    // TODO ask for end time (if any)
     Date start = mag.getDateEnreg();
     Date end = mag.getDateEnreg();
 
@@ -58,50 +61,10 @@ public class F31_Performance extends DoremusResource {
     // Performance: Title
     this.resource.addProperty(RDFS.label, item.getLabel());
     this.resource.addProperty(CIDOC.P102_has_title, item.getLabel());
-    System.out.println(item.getLabel());
-
-    // Performance: General Topic
-    List<ItemIdxFile> generalTopics = ItemIdxNc.byItem(item.getId());
-    generalTopics.addAll(ItemIdxRencontre.byItem(item.getId()));
-    for (ItemIdxFile t : generalTopics) {
-      try {
-        String uri = ConstructURI.build("rfi", "theme", t.getIdxId()).toString();
-
-        Resource topic = model.createResource(uri)
-          .addProperty(RDF.type, CIDOC.E1_CRM_Entity)
-          .addProperty(DCTerms.identifier, t.getIdxId())
-          .addProperty(RDFS.label, t.getIdxLabel());
-        this.resource.addProperty(CIDOC.P129_is_about, topic);
-
-      } catch (URISyntaxException e) {
-        e.printStackTrace();
-      }
-    }
-
-    // Performance: Geo Topic
-    // FIXME the property U22 is not yet in the ontology
-//        for (ItemIdxGeo t : ItemIdxGeo.byOmu(item.getId())) {
-//            E53_Place p = new E53_Place(t.getLieuGeoId());
-//            this.resource.addProperty(MUS.U22_is_about_place, p.asResource());
-//        }
+//    System.out.println(item.getLabel());
 
 
-    // Performance: Person + Corporate Body
-    // create list of persons
-    List<Resource> actors = ItemIdxPersonne.byItem(item.getId()).stream()
-      .map(it -> new E21_Person(it.getIdxId()).asResource())
-      .collect(Collectors.toList());
 
-    // add corporate bodies
-    actors.addAll(ItemIdxMorale.byItem(item.getId()).stream()
-      .map(it -> new F11_Corporate_Body(it.getIdxId()).asResource())
-      .collect(Collectors.toList()));
-
-
-    // for (Resource x : actors) {
-    // FIXME the property U21 is not yet in the ontology
-//     this.resource.addProperty(MUS.U21_is_about_actor, x.asResource());
-    // }
 
     // Performance: comment
     for (String s : new String[]{item.getDescription(), item.getAnalyseDoc()}) {
